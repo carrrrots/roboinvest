edb7ffb6d256a9698213f51eac73ef532902fa89
Alyssa Coelho <alyssagcoelho@gmail.com>
2021-11-30 14:14:45 -0300

    stock controller/view criado sem update

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,20 @@
+class StocksController < ApplicationController
+before_action :set_stock, only: %i[show]
+
+  def index
+    @stocks = Stock.all
+  end
+
+  def show
+  end
+
+private
+
+  def stock_params
+    params.require(:stock).permit(:name, :symbol)
+  end
+
+  def set_stock
+    @stock = Stock.find(params[:id])
+  end
+end
