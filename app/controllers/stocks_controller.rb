9221439664c1d95d79219b3e8f17fa62640d4c53
Alyssa Coelho <alyssagcoelho@gmail.com>
2021-11-30 17:24:55 -0300

    API criada

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,28 @@
+require 'open-uri'
+
 class StocksController < ApplicationController
 before_action :set_stock, only: %i[show]
 
   def index
-    @stocks = Stock.all
+    @stocks = open_market
   end
 
   def show
+  end
+
+  def open_market
+    stocks = {}
+    STOCKS_SYMBOLS.keys.each do |symbol|
+      url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=#{ENV['ALPHA_API_KEY']}"
+      stock_serialized = URI.open(url).read
+      stock = JSON.parse(stock_serialized)
+      stocks[symbol] = {} if stocks[symbol].nil?
+      stocks[symbol]["BookValue"] = stock["BookValue"]
+      stocks[symbol]["Country"] = stock["Country"]
+      stocks[symbol]["Exchange"] = stock["Exchange"]
+    end
+
+    stocks
   end
 
 private
