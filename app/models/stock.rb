a224ccd3c6142bf22dfc1ab90698ecc7bc3af553
Caroline Rezende <caroline6254@hotmail.com>
2021-12-01 14:34:01 -0300

    api fetch ok

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,17 @@
+require 'open-uri'
+
 class Stock < ApplicationRecord
+
+   def refresh
+    stocks = {}
+    url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=#{self.symbol}&apikey=#{ENV['ALPHA_API_KEY']}"
+    stock_serialized = URI.open(url).read
+    stock = JSON.parse(stock_serialized)
+    stocks[symbol] = {} if stocks[symbol].nil?
+    stocks[symbol]["BookValue"] = stock["BookValue"]
+    stocks[symbol]["Country"] = stock["Country"]
+    stocks[symbol]["Exchange"] = stock["Exchange"]
+    stocks
+  end
+
 end
