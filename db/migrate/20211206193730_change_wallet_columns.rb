5bfa5284b4dfe84ee51b618384c7f2b9b0086fd4
Alyssa Coelho <alyssagcoelho@gmail.com>
2021-12-06 23:28:24 -0300

    funcoes adicionadas

--- db/migrate/20211206193730_change_wallet_columns.rb
+++ db/migrate/20211206193730_change_wallet_columns.rb
@@ -1,7 +1,7 @@
 class ChangeWalletColumns < ActiveRecord::Migration[6.1]
   def change
     remove_column :wallets, :money, :decimal
-    change_column :wallets, :invested_money, :float
-    change_column :wallets, :profit, :float
+    change_column :wallets, :invested_money, :float, default: 0.00
+    change_column :wallets, :profit, :float, default: 0.00
   end
 end
